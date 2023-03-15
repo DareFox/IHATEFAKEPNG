aff9a12f734e75ea50f03be9a54991056afade6e
DareFox <47672780+DareFox@users.noreply.github.com>
2023-03-15 18:33:46 +0300

    Bing: add bing manager

---
+++ src/websites/bing.ts
@@ -0,0 +1,44 @@
+import { combineParamsAndUrl, isDomain, isUserSearchingTransparent } from "./common";
+import { WebsiteUrlConverter } from "./websiteUrlConverter";
+
+const BingUrlConverter: WebsiteUrlConverter = {
+    name: "Bing",
+    isUrlValid: function (url: URL): boolean {
+        return isDomain(url, "bing.com") && isSearchingImages(url) && isUrlAlreadyTransparent(url) && isQueryTriggers(url)
+    },
+    convertURL: function (url: URL): URL {
+        const newUrlParams = new URLSearchParams(url.search)
+        var settings = newUrlParams.get("qft")?.split("+")
+
+        if (!settings) {
+            settings = [''] // needed for adding plus at start
+        }
+
+        settings.filter((value, index, array) => {
+            !value.startsWith("filterui:photo-") // filter out any photo filters
+        })
+
+        settings.push("filterui:photo-transparent")
+        newUrlParams.set("iaf", settings.join('+'))
+
+        return combineParamsAndUrl(url, newUrlParams)
+    }
+}
+
+function isSearchingImages(url: URL): boolean {
+    return url.searchParams.get("form") == "HDRSC3"
+}
+
+function isQueryTriggers(url: URL): boolean {
+    return isUserSearchingTransparent(url.searchParams.get("q"))
+}
+
+function isUrlAlreadyTransparent(url: URL): boolean {
+    const param = url.searchParams.get("qft")
+
+    if (!param)
+        return false
+
+    return param.includes("+filterui:photo-transparent")
+}
+
