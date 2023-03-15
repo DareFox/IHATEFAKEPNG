a93a5c1ea5d2b5b3565142228628d22c09b50587
DareFox <easymalink@gmail.com>
2023-03-15 20:12:52 +0300

    Extension: add function to set/get websites

--- src/options.ts
+++ src/options.ts
@@ -1,5 +1,12 @@
 import { isStringsArray } from "./typeChecks"
+import { BingUrlConverter } from "./websites/bing"
+import { DuckDuckGoUrlConverter } from "./websites/duckduckgo"
+import { GoogleUrlConverter } from "./websites/google"
+import { WebsiteUrlConverter } from "./websites/websiteUrlConverter"
+import { YahooUrlConverter } from "./websites/yahoo"
+import { YandexUrlConverter } from "./websites/yandex"
 
+const allWebsites = [GoogleUrlConverter, BingUrlConverter, DuckDuckGoUrlConverter, YahooUrlConverter, YandexUrlConverter]
 const defaultWords = ["png"]
 
 export async function getWords(): Promise<string[]> {
@@ -30,3 +37,47 @@ export async function setWords(words: string[]): Promise<void> {
         "words": words
     })
 }
+
+export async function setWebsites(websites: WebsiteUrlConverter[]): Promise<void> {
+    const websitesNames = websites.map((value) => value.name)
+
+    return chrome.storage.sync.set({
+        "websites": websitesNames
+    })
+}
+
+export function websitesNamesToObject(websiteNames: string[]): WebsiteUrlConverter[] {
+    const websiteMap = new Map(allWebsites.map((site) => [site.name, site]))
+
+    return websiteNames.flatMap(name => {
+        const site = websiteMap.get(name)
+
+        if (site) return [site] 
+        else return [] // after flattening empty objects (converted nulls) will be removed
+    })
+}
+
+
+export async function getWebsites(): Promise<WebsiteUrlConverter[]> {
+    try {
+        const value = (await chrome.storage.sync.get("websites"))?.websites
+        
+        if(!value || !isStringsArray(value)) {
+            setWebsites(allWebsites).then(() => {
+                console.log("Updated storage to default websites successfully")
+            }).catch(() => {
+                console.log("Failed to update storage to default websites")
+            })
+ 
+            return allWebsites
+        }
+
+        return websitesNamesToObject(value)
+    } catch (ex) {
+        console.error(ex)
+        console.error("Returning all websites")
+
+        return allWebsites
+    }
+}
+
