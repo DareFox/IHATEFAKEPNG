6636ad91660212c0ee905939d546589d2ea0ce0b
DareFox <47672780+DareFox@users.noreply.github.com>
2023-03-16 14:53:39 +0300

    Webpack: copy html to out/

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ const srcDir = path.join(__dirname, "src");
 module.exports = {
     entry: {
       background: path.join(srcDir, 'background.ts'),
+      optionsHtmlHandler: path.join(srcDir, 'optionsHtmlHandler.ts'),
+    //   optionsHtml: path.join(srcDir, 'options.html')
     },
     output: {
         path: path.join(__dirname, "./out"),
@@ -29,13 +31,15 @@ module.exports = {
         ],
     },
     resolve: {
-        extensions: [".ts", ".tsx", ".js"],
+        extensions: [".ts", ".tsx", ".js", ".html"],
     },
     plugins: [
-        // new CopyPlugin({
-        //     patterns: [{ from: ".", to: "../", context: "public" }],
-        //     options: {},
-        // }),
+        new CopyPlugin({
+            patterns: [{ context: srcDir, from: path.join(srcDir, "*.html")}],
+            options: {
+
+            },
+        }),
     ],
     devtool: 'inline-source-map',
     mode: 'development'
