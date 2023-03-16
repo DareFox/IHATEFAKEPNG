2a4d3cd67d6ded2567447707e60c1165e45e78f7
DareFox <47672780+DareFox@users.noreply.github.com>
2023-03-16 12:33:33 +0300

    Extension: add function to save previous urls for comparing

---
+++ src/previousUrl.ts
@@ -0,0 +1,14 @@
+const previousUrlMap: Record<number,URL> = {};
+
+export function updatePreviousUrl(id: number, url: URL) {
+    previousUrlMap[id] = url
+}
+
+export function removePreviousUrl(id: number) {
+    delete previousUrlMap[id]
+}
+
+export function getPreviousUrl(id: number): URL | undefined {
+    return previousUrlMap[id]
+}
+
