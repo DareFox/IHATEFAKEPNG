5c23c170f0c49e1519a0e468e4008fc4143e8d6b
DareFox <easymalink@gmail.com>
2023-03-16 12:34:19 +0300

    Extension: on query change, recheck url

--- src/background.ts
+++ src/background.ts
@@ -12,6 +12,30 @@ chrome.tabs.onUpdated.addListener(async (tabID, changeInfo, tab) => {
     var urlObj = new URL(url)
 
     for (const website of await getWebsites()) {
+        // if previous url was changed by extension
+        // and new url can remove filter transparency
+        // then new url was set by previous url
+        if (previousUrl && isConvertedByExtension(previousUrl) && website.canUndoConvertUrl(urlObj)) {
+            // remove filter from new url
+            const cleanUrl = changeParams(website.undoConvertUrl(urlObj), params => {
+                params.delete("removefakepng")
+            })
+            const isValidForConverstion = await website.canConvertUrl(cleanUrl)
+
+            // if new url doesn't trigger transparency filter
+            // then set clean url without previous filters
+            if (!isValidForConverstion) {
+                chrome.tabs.update(tabID, {
+                    url: cleanUrl.toString()
+                })
+
+                console.log(`Cleaning new URL from previous URL. Redirecting from ${url} to ${cleanUrl.toString()}`)
+                updatePreviousUrl(tabID, cleanUrl)
+                return
+            }
+        }
+
+
         if (await website.canConvertUrl(urlObj)) {
             const newUrl = changeParams(await website.convertUrl(urlObj), params => {
                 params.set("removefakepng", "true")
